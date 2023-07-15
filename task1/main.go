2fb94a7105f14e915e2c75e40b448d0e1e12003b
Anton <anton.git.dev@gmail.com>
2023-07-15 14:50:39 +0300

    Remove comments

--- task1/main.go
+++ task1/main.go
@@ -83,32 +83,3 @@ func main() {
 		}
 	}
 }
-
-//3 3 12
-//DISABLE 1 2
-//DISABLE 2 1
-//DISABLE 3 3
-//GETMAX
-//RESET 1
-//RESET 2
-//DISABLE 1 2
-//DISABLE 1 3
-//DISABLE 2 2
-//GETMAX
-//RESET 3
-//GETMIN
-//
-//1
-//2
-//1
-
-//2 3 9
-//DISABLE 1 1
-//DISABLE 2 2
-//RESET 2
-//DISABLE 2 1
-//DISABLE 2 3
-//RESET 1
-//GETMAX
-//DISABLE 2 1
-//GETMIN
